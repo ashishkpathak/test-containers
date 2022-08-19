9571c5788bc720be42c7f008ad29c37b138292e9
Ashish Pathak <ashish.pathak@optus.com.au>
2022-08-19 13:52:54 +1000

    updated test

--- src/main/java/au/id/pathak/testcontainers/config/TestContainersConfiguration.java
+++ src/main/java/au/id/pathak/testcontainers/config/TestContainersConfiguration.java
@@ -43,4 +43,6 @@ public class TestContainersConfiguration {
     template.afterPropertiesSet();
     return template;
   }
+
+
 }
--- src/test/java/au/id/pathak/testcontainers/redis/TraditionalRedisTests.java
+++ src/test/java/au/id/pathak/testcontainers/redis/TraditionalRedisTests.java
@@ -1,67 +1,17 @@
 package au.id.pathak.testcontainers.redis;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import io.swagger.parser.OpenAPIParser;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.PathItem;
-import io.swagger.v3.oas.models.media.Content;
-import io.swagger.v3.oas.models.media.MediaType;
-import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.parameters.Parameter;
-import io.swagger.v3.oas.models.parameters.RequestBody;
-import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@RunWith(SpringRunner.class)
 public class TraditionalRedisTests {
 
-  @Autowired
-  private RedisTemplate template;
 
   @Test
   public void testRedis() {
-    SwaggerParseResult result = new OpenAPIParser().readLocation("https://petstore3.swagger.io/api/v3/openapi.json", null, null);
-
-    // or from a file
-    // SwaggerParseResult result = new OpenAPIParser().readLocation("./path/to/openapi.yaml", null, null);
-
-    // the parsed POJO
-    OpenAPI openAPI = result.getOpenAPI();
-    Set<String> keySet = openAPI.getPaths().keySet();
-    for (String s : keySet) {
-      System.out.println(s);
-      PathItem pathItem = openAPI.getPaths().get(s);
-      Operation post = pathItem.getPost();
-      if (post!=null){
-        RequestBody requestBody = post.getRequestBody();
-        Content content = requestBody.getContent();
-        Set<String> strings = content.keySet();
-        for (String contentKey : strings) {
-          MediaType mediaType = content.get(contentKey);
-          Schema schema = mediaType.getSchema();
-          Map properties = schema.getProperties();
-          if(properties!=null)
-          properties.forEach((x,y)->{System.out.println(x+" " + y);});
-        }
-        System.out.println(content);
-      }
-      List<Parameter> parameters = pathItem.getParameters();
-      if (parameters != null)
-        for (Parameter parameter : parameters) {
-          System.out.println(parameter);
-        }
-    }
-    openAPI.getPaths().forEach((y, x) -> {
-      System.out.println(x + " " + y);
-    });
-
+    System.out.println("Hello");
   }
 }
