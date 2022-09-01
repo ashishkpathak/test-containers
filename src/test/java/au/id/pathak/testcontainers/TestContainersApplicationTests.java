35a919b667ba8f84ae5340c4244508685b181404
Ashish Pathak <ashish.pathak@optus.com.au>
2022-09-02 09:24:48 +1000

    plain test updates.

--- src/test/java/au/id/pathak/testcontainers/TestContainersApplicationTests.java
+++ src/test/java/au/id/pathak/testcontainers/TestContainersApplicationTests.java
@@ -4,49 +4,32 @@ import java.util.List;
 
 import au.id.pathak.testcontainers.db.Artist;
 import au.id.pathak.testcontainers.db.ArtistDao;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.util.Assert;
 
 import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 
 @SpringBootTest
-@Testcontainers
 class TestContainersApplicationTests {
 
   @Autowired
   private ArtistDao artistDao;
 
-//docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
-  @Container
-  private static PostgreSQLContainer container = (PostgreSQLContainer) new PostgreSQLContainer("postgres:latest");
-
-  @DynamicPropertySource
-  public static void overrideProperties(DynamicPropertyRegistry registry){
-    registry.add("spring.datasource.username", container::getUsername);
-    registry.add("spring.datasource.password", container::getPassword);
-    registry.add("spring.datasource.database", container::getDatabaseName);
-    registry.add("spring.datasource.url", container::getJdbcUrl);
-  }
 
   @Test
   public void test_all_artists_exists() {
     List<Artist> all = artistDao.findAll();
     all.forEach(System.out::println);
-    Assert.assertTrue(all.size() == 6);
+    Assert.isTrue(all.size() == 6, "Size should be 6");
   }
 
   @Test
   public void test_van_gogh_exists() {
     List<Artist> all = artistDao.findAll();
     long van_gogh = all.stream().filter(artist -> containsIgnoreCase(artist.getLastName(), "van gogh")).count();
-    Assert.assertTrue(van_gogh == 1);
+    Assert.isTrue(van_gogh == 1, "Should have single entry.");
   }
 
 }
--- src/test/java/au/id/pathak/testcontainers/TestContainersSpringSupportTests.java
+++ src/test/java/au/id/pathak/testcontainers/TestContainersSpringSupportTests.java
@@ -4,38 +4,33 @@ import java.util.List;
 
 import au.id.pathak.testcontainers.db.Artist;
 import au.id.pathak.testcontainers.db.ArtistDao;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.util.Assert;
 
 import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 
-@SpringBootTest(properties = {"spring.datasource.url=jdbc:tc:postgresql:latest:///gomo"})
-@Testcontainers
+@SpringBootTest(properties = {"spring.datasource.url=jdbc:tc:postgresql:9.6.8:///gomo"})
 public class TestContainersSpringSupportTests {
 
   @Autowired
   private ArtistDao artistDao;
 
 
-//  @Test
-//  public void test_all_artists_exists() {
-//    List<Artist> all = artistDao.findAll();
-//    Assert.assertTrue(all.size() == 6);
-//    all.forEach(System.out::println);
-//  }
-//
-//  @Test
-//  public void test_van_gogh_exists() {
-//    List<Artist> all = artistDao.findAll();
-//    boolean vanGogh = false;
-//    long van_gogh = all.stream().filter(artist -> containsIgnoreCase(artist.getLastName(), "van gogh")).count();
-//    Assert.assertTrue(van_gogh == 1);
-//  }
+  @Test
+  public void test_all_artists_exists() {
+    List<Artist> all = artistDao.findAll();
+    Assert.isTrue(all.size() == 6, "Should have size of 6.");
+
+    all.forEach(System.out::println);
+  }
+
+  @Test
+  public void test_van_gogh_exists() {
+    List<Artist> all = artistDao.findAll();
+    boolean vanGogh = false;
+    long van_gogh = all.stream().filter(artist -> containsIgnoreCase(artist.getLastName(), "van gogh")).count();
+    Assert.isTrue(van_gogh == 1,"Should have single element.");
+  }
 }
