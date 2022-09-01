43232c736ecdd5a5b447e0011fdffc9025555f14
Ashish Pathak <ashish.pathak@optus.com.au>
2022-09-02 09:48:01 +1000

    added test container support.

--- src/test/java/au/id/pathak/testcontainers/TestContainersApplicationTests.java
+++ src/test/java/au/id/pathak/testcontainers/TestContainersApplicationTests.java
@@ -7,16 +7,32 @@ import au.id.pathak.testcontainers.db.ArtistDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.util.Assert;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 
 @SpringBootTest
+@Testcontainers
 class TestContainersApplicationTests {
 
   @Autowired
   private ArtistDao artistDao;
 
+  @Container
+  private static PostgreSQLContainer container = new PostgreSQLContainer("postgres:latest");
+
+
+  @DynamicPropertySource
+  public static void override(DynamicPropertyRegistry registry){
+    registry.add("spring.datasource.username", container::getUsername);
+    registry.add("spring.datasource.password", container::getPassword);
+    registry.add("spring.datasource.url", container::getJdbcUrl);
+  }
 
   @Test
   public void test_all_artists_exists() {
